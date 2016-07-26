aa08f2111a6f75fce8c8d559bcb8584ef5b6f2f6
Devin <devinhaber@yahoo.com>
2016-07-25 22:42:12 -0700

    Initial

---
+++ app.js
@@ -0,0 +1,95 @@
+var Discord = require('discord.js');
+var config = require('./config.json');
+var orm = require('orm');
+var dbmanage = require('./dbmanage.js')
+
+var bot = new Discord.Client({autoReconnect: true});
+
+var users = {}
+
+function playFile(channel, filename) {
+    if (channel == null || filename == '') {
+        return;
+    } else {
+        bot.joinVoiceChannel(channel, (err, connection) => {
+            console.log('joining channel ' + channel.name)
+            path = './sounds/' + filename + '.mp3' //More file formats coming in the future!
+            options = 
+            {
+                'volume': config.volume
+            }
+            connection.playFile(path, options, (err, intent) => {
+                intent.on('end', () => {connection.destroy()});
+                if (err) {
+                    console.log(err)
+                }
+            })
+        })
+    }
+}
+function executeMessage(cmd) {
+    if (cmd.content == '!help') {
+        bot.sendMessage(cmd, config.helpMessage, (err, msg) => {return;});
+    } else if (cmd.content.substring(0,5) == "!play") {
+        playFile(cmd.author.voiceChannel, cmd.content.substring(6));
+    } else if (cmd.content == '!stop') {
+        if (bot.voiceConnection) {
+            bot.voiceConnection.destroy();
+        }
+    } 
+}
+function processMessage(msg) {
+    if (msg.content.substring(0,1) == '!') {
+        //for (user in config.authorizedUsers) {
+            //for (i = 0; i < config.authorizedUsers.length; i++) {
+                //if (config.authorizedUsers[i] == msg.author.username) {
+                    executeMessage(msg);
+                    return;
+                //}
+            //}
+        //}
+         
+    }
+}
+bot.on("message", msg => {
+    processMessage(msg);})
+
+bot.on("ready", () => {
+    //Update for funny game message
+    bot.setPlayingGame(config.playingGame, (err) => {if (err) {console.log(err);}})
+    //On joining, store everyones roles for usage
+    for (i=0;i<bot.servers.length;i++) {
+        server = bot.servers[i];
+        for (mem in server.memberMap) {
+            users[server.name + mem] = server.memberMap[mem]['roles'];
+        }
+    }
+    orm.connect(config.DBConnection, (err, db) => {
+        if (err) throw err;
+        dbmanage.setSchema(orm, db);
+    })
+})
+
+bot.on("serverMemberUpdated", (server, newUser, oldUser) => {
+    //Whenever a user is modified, update his roles
+    users[server.name + newUser.id] = server.rolesOfUser(newUser);
+})
+
+bot.on("serverNewMember", (server, user) => {
+    //If a user is added, change his roles to the cached version
+    found = false;
+    for (prop in users) {
+        if ((server.name + user.id) == prop) {
+            roles = users[server.name + user.id];
+            bot.addUserToRole(user, roles, (err) => {if (err) {console.log(err);}});
+            found = true;
+        }
+    }
+    //If we can't find the cached version, then we store their roles
+    if (!found) {
+        users[server.name + user.id] = server.rolesOfUser(user)
+    }
+})
+
+
+bot.loginWithToken(config.token)
---
+++ dbmanage.js
@@ -0,0 +1,42 @@
+var tables = {}
+
+var races = ['orc', 'pandaren', 'goblin', 'worgen', 'draenei', 'blood_elf','dwarf',
+             'human', 'gnome', 'tauren', 'troll', 'night_elf', 'undead']
+var classes = ['warrior', 'paladin', 'hunter', 'rogue', 'priest', 'death_knight', 'shaman',
+               'mage', 'warlock', 'monk', 'demon_hunter', 'druid']
+var roles = ['dps', 'tank', 'healer']
+
+var professions = ['engineering', 'tailoring', 'alchemy', 'blacksmithing', 'enchanting', 'herbalism', 'inscription', 'jewelcrafting', 'leatherworking', 'mining', 'skinning']
+
+exports.setSchema = (orm, db) => {
+    var Raider = db.define('raider', {
+        id:   {type:'serial', key:true},
+        realname:    {type: 'text'},
+        name:       {type: 'text'},
+        realm:      {type: 'text'},
+        faction:    {type: 'enum', values: ['alliance', 'horde']},
+        race:       {type: 'enum', values: races},
+        class:      {type: 'enum', values: classes},
+        role:       {type: 'enum', values: roles},
+        profession1: {type: "enum", values: professions},
+        profession2: {type: "enum", values: professions},
+    });
+    var Raid = db.define('raid', {
+        id:     {type: 'serial', key: true},
+        name:   {type: 'text'},
+        raid:   {type: 'text'},
+    });
+    var Schedule = db.define('schedule', {
+        id:     {type: 'serial', key: true},
+        name:   {type: 'text'},
+        time:   {type: 'text'},
+    });
+    Raid.hasMany('raiders', Raider, {}, {reverse: 'raids', key:true});
+    Raid.hasOne('schedule', Schedule, {reverse: 'raid'});
+    db.sync((err) => {if (err) throw err;});
+    tables['raid'] = Raid;
+    tables['raider'] = Raider;
+    tables['Schedule'] = Schedule;
+}
+
+exports.tables = tables
